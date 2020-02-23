b6caca74355629d67fe1a9a0e1b50be1f265db04
Naoyuki Sogo <sogo@longtail-software.co.jp>
2020-02-23 01:52:51 +0000

    add test case for ls

--- test/sftp.js
+++ test/sftp.js
@@ -132,6 +132,11 @@ describe("test for sftp subcommands", function() {
     it("should return only matched filenames src contains multipl glob pattern", async()=>{
       expect(await arssh.ls(path.posix.join(remoteRoot, "h*", "p[iu]yo"))).to.have.members([path.posix.join(remoteRoot, "hoge", "piyo"), path.posix.join(remoteRoot, "hoge", "puyo")]);
     });
+    it.only("should return only matched filenames, if specified glob contains /", async()=>{
+      expect(await arssh.ls(path.posix.join(remoteRoot, "hoge/*yo"))).to.have.members(["piyo", "puyo", "poyo"].map((e)=>{
+        return path.posix.join(remoteRoot, e);
+      }));
+    });
   });
   describe("#chmod", function() {
     this.timeout(4000);
